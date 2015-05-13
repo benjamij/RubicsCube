6124466bc8fb9cd97cf8ef36af93772e8c15c821
fernandus16 <fernando.correia@jusbrasil.com.br>
2015-05-13 14:54:10 -0300

    Improvements: graph.py

--- graph.py
+++ graph.py
@@ -1,7 +1,5 @@
 import networkx as nx
 from sympy.combinatorics.permutations import Permutation
-from math import factorial
-import random
 
 from cube import RubiksCube
 from cube import move_rubiks_cube
@@ -27,6 +25,7 @@ class Node(object):
 class RCGraph(object):
     """ Represents the rubik's cube in graph form. Relies on networkx.
     """
+
     def __init__(self):
         """ Constructor. Initializes the graph with a solved cube.
         """
@@ -44,14 +43,13 @@ class RCGraph(object):
             Return:
                 List of adjacent nodes.
         """
-        nodes = self._get_possible_adjacents(node)
-        while(not self._is_valid_adjacents(nodes)):
-            nodes = self._get_possible_adjacents(node)
-        for n in nodes:
-            self._add_node(n, node)
+        nodes = filter(lambda node: not self._graph.has_node(node),
+                       self._get_adjacents(node))
+
+        self._graph.add_nodes_from(nodes)
         return nodes
 
-    def _get_possible_adjacents(self, node):
+    def _get_adjacents(self, node):
         """ Returns a possible list of adjacents. Checks whether state is valid
             upon generation.
 
@@ -62,43 +60,17 @@ class RCGraph(object):
             Return:
                 List of successor states.
         """
-        # List of 12 or 11 random movements, one for each child
         p_mov = node.parent_mov
+        n_mov = node.mov
+        n_cube = node.rubiks_cube
 
-        # The permutation of 12 movements (0, 1, 2, ..., 11) to ensure that
-        # there is no equal adjacent nodes
-        moves = Permutation.unrank_nonlex(12, random.randint(1,
-                                          factorial(12))).list()
+        # Removes the movement which creates a adjacent equals to the node's
+        # parent
+        moves = filter(lambda n: n != p_mov,
 
-        if p_mov > 0:
-            # Removes the movement which creates a adjacent equals to the
-            # node's parent
-            moves.remove(p_mov)
+                       # The permutation of 12 movements (0, 1, 2, ..., 11) to
+                       #ensure that there is no equal adjacent nodes
+                       Permutation.unrank_nonlex(12, 479001600).list())
 
-        return [Node(move_rubiks_cube(node.rubiks_cube, m), m, node.mov) for m in moves]
-
-    def _is_valid_adjacents(self, nodes):
-        """ Checks if the generated adjacent is valid,
-            the list of nodes will be valid if it none of its nodes already exist \
-            in the graph.
-
-            Args:
-                nodes       The list of nodes whose validity to check.
-        """
-        for rb in nodes:
-            if self._graph.has_node(rb):
-                return False
-        return True
-
-    def _add_node(self, node, parent=None):
-        """ Adds a new node in the graph.
-
-            Args:
-                node        The node that is to be added to the graph.
-                parent      (Optional) parent of the node to attach to the graph.
-        """
-        self._graph.add_node(node)
-        if parent:
-            self._graph.add_edge(node, parent)
-        else:
-            self._graph.add_edge(node, self.root)
+        return map(lambda m: Node(move_rubiks_cube(n_cube, m), m, n_mov),
+                   moves)
